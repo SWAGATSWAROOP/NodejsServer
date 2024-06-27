452759239be7fe006d2208983c130bf37999f474
Pranjal Pandey <pranjalpandey@Pranjals-MacBook-Air.local>
2024-06-27 14:21:26 +0530

    indexing

--- src/mongodb/db.js
+++ src/mongodb/db.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
-
+require("dotenv").config()
 const connectToDB = async () => {
   try {
+    console.log(process.env.DB_URI,"line 5")
     await mongoose.connect(process.env.DB_URI);
+    console.log("connected")
   } catch (error) {
     console.log("Error in connecting to DB");
   }
